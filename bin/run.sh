8debf62c4655ae0da8d98a9a8c42b335c84b9384
Pavel Yuval Gak <yuvalgak@gmail.com>
2018-09-25 12:33:32 +0300

    run.sh: adding mkdir output.

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 rm -rf ./input
+mkdir output
 curl -o input.zip https://raw.githubusercontent.com/AthenaWisdom/standalone_scorer/master/bin/input.zip
-unzip input.zip
+unzip -o input.zip
 docker run --mount type=bind,source="$(pwd)"/input,destination=/app/input,consistency=cached --mount type=bind,source="$(pwd)"/output,destination=/app/output,consistency=cached endor/scorer:latest
 
