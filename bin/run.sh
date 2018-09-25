32201426fbcd9dad62e59174a2dd7bf39dcc4ea3
Pavel Yuval Gak <yuvalgak@gmail.com>
2018-09-25 12:30:20 +0300

    run.sh: update.

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 rm -rf ./input
-curl -o input.zip https://raw.githubusercontent.com/AthenaWisdom/standalone_scorer/master/bin/input.
-unzip input.zip .
+curl -o input.zip https://raw.githubusercontent.com/AthenaWisdom/standalone_scorer/master/bin/input.zip
+unzip input.zip
 docker run --mount type=bind,source="$(pwd)"/input,destination=/app/input,consistency=cached --mount type=bind,source="$(pwd)"/output,destination=/app/output,consistency=cached endor/scorer:latest
 
