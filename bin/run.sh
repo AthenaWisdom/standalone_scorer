728f8b15aee19e4f6344b429729f387a7c383b21
Pavel Yuval Gak <yuvalgak@gmail.com>
2018-09-25 12:20:26 +0300

    adding bin/input.zip

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -x
 rm -rf ./input
-cp -R ../in ./
-mv ./in ./input
-mv ./input/in ./input/input
+curl -o input.zip https://raw.githubusercontent.com/AthenaWisdom/standalone_scorer/master/bin/input.
+unzip input.zip .
 docker run --mount type=bind,source="$(pwd)"/input,destination=/app/input,consistency=cached --mount type=bind,source="$(pwd)"/output,destination=/app/output,consistency=cached endor/scorer:latest
 
